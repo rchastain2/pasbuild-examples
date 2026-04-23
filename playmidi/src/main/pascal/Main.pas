334bbd2ed7cca5884b5ba283e3203270caa509ce
Roland Chastain <roland.chastain@gmx.fr>
2026-04-23 11:31:47 +0200

    Add profile

--- playmidi/src/main/pascal/Main.pas
+++ playmidi/src/main/pascal/Main.pas
@@ -1,6 +1,7 @@
 
 program PlayMidi;
 {$MODE objfpc}{$H+}
+{$DEFINE demo}
 
 uses
 {$IFDEF unix}
@@ -21,6 +22,7 @@ end;
 
 const
   CFont ='SC-55 SoundFont v1.2b.sf2';
+  CDemoFile = 'sarabande.mid';
 
 var
   fluidsynth: TFluidSynth = nil;
@@ -31,31 +33,40 @@ var
 begin
   SetHeapTraceOutput('heaptrc.log');
   
-  if FileExists(ParamStr(1)) and (ExtractFileExt(ParamStr(1)) = '.mid') then
+{$IFDEF demo}
+  filepath := ExtractFilePath(ParamStr(0)) + CDemoFile;
+{$ELSE}
+  filepath := ParamStr(1);
+{$ENDIF}
+  
+  if not FileExists(filepath) then
   begin
-    filepath := ParamStr(1);
-    fontpath := ExtractFilePath(ParamStr(0)) + CFont;
-    
-    fluidsynth := TFluidSynth.Create();
-    try
-      WriteLn('[DEBUG] ', fluidsynth.status);
-      if not fluidsynth.isLoaded then
-        Exit;
-      with fluidsynth do
-      begin
-        settings := new_fluid_settings();
-        synth := new_fluid_synth(settings);
-        soundfontid := fluid_synth_sfload(synth, pchar(fontpath), 1);
-        audioDriver := new_fluid_audio_driver(settings, synth);
-        player := new_fluid_player(synth);
-        fluid_player_add(player, pchar(filepath));
-        fluid_player_play(player);
-      end;
-      // Let the user listen to the song! Maybe wait for a keypress to exit.
-      WaitForKeyPress;
-    finally
-      fluidsynth.Destroy;
-    end;
-  end else
     WriteLn('Usage: ', ParamStr(0), ' MIDI_FILE');
+    Exit;
+  end;
+  
+  WriteLn('[INFO] File: ', filepath);
+  
+  fontpath := ExtractFilePath(ParamStr(0)) + CFont;
+  
+  fluidsynth := TFluidSynth.Create();
+  try
+    WriteLn('[DEBUG] ', fluidsynth.status);
+    if not fluidsynth.isLoaded then
+      Exit;
+    with fluidsynth do
+    begin
+      settings := new_fluid_settings();
+      synth := new_fluid_synth(settings);
+      soundfontid := fluid_synth_sfload(synth, pchar(fontpath), 1);
+      audioDriver := new_fluid_audio_driver(settings, synth);
+      player := new_fluid_player(synth);
+      fluid_player_add(player, pchar(filepath));
+      fluid_player_play(player);
+    end;
+    // Let the user listen to the song! Maybe wait for a keypress to exit.
+    WaitForKeyPress;
+  finally
+    fluidsynth.Destroy;
+  end;
 end.
